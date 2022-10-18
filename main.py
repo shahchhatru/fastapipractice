4d8ee2c9051a265c58f6e31f5a86cc00925c0ed2
Khesav Chhatra <shahkhesavchhatra@gmail.com>
2022-10-18 11:09:30 +0545

    commit before starting more:

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.params import Body
+from typing import Optional
 
 app = FastAPI()
 
 
+class Item(BaseModel):
+    name:str
+    description:str|None=None
+    rating:Optional[int]=None
 
 @app.get("/")
 async def home():
@@ -15,3 +20,10 @@ async def home():
 async def create_post(payload:dict=Body(...)):
     print(payload)
     return {"message":f"The title of post is {payload['title']} and content is {payload['content']}"}
+
+
+@app.post("/createposts/")
+async def create_posts(item:Item):
+    print(item.dict)
+    print(item)
+    return {"data":item}
